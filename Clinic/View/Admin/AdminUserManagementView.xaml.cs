065395c5fff280692c1fedc80a64d197f7a0385a
ShulhaOleh <broo20544@gmail.com>
2026-04-14 14:48:34 -0400

    Add Edit button to admin UI and remove duplicate DB logic from view

--- Clinic/View/Admin/AdminUserManagementView.xaml.cs
+++ Clinic/View/Admin/AdminUserManagementView.xaml.cs
@@ -2,10 +2,6 @@
 using System.Windows.Controls;
 using System.Windows.Input;
 using Clinic.Models;
-using Clinic.View.Admin;
-using Clinic.DB;
-using MySql.Data.MySqlClient;
-using System;
 using Clinic.ViewModels.Admin;
 
 namespace Clinic.View.Admin
@@ -38,38 +34,17 @@ namespace Clinic.View.Admin
 
 
 
+        private void EditUser_Click(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is AdminUserManagementViewModel vm)
+                vm.EditUser(vm.SelectedUser);
+        }
+
         private void UserGrid_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
-            if ((sender as DataGrid)?.SelectedItem is User user)
-            {
-                var dialog = new EditUserDialog(user);
-                if (dialog.ShowDialog() == true)
-                {
-                    using var conn = AuthDB.GetConnection();
-                    conn.Open();
-
-                    var cmd = new MySqlCommand(@"
-                        UPDATE Users 
-                        SET Username = @u, Role = @r, LinkedID = @l
-                        " + (string.IsNullOrWhiteSpace(dialog.Password) ? "" : ", PasswordHash = MD5(@p)") + @"
-                        WHERE UserID = @id", conn);
-
-                    cmd.Parameters.AddWithValue("@u", dialog.Username);
-                    cmd.Parameters.AddWithValue("@r", dialog.Role);
-                    cmd.Parameters.AddWithValue("@l", dialog.LinkedID.HasValue ? dialog.LinkedID.Value : DBNull.Value);
-                    cmd.Parameters.AddWithValue("@id", user.UserID);
-
-                    if (!string.IsNullOrWhiteSpace(dialog.Password))
-                        cmd.Parameters.AddWithValue("@p", dialog.Password);
-
-                    cmd.ExecuteNonQuery();
-                }
-
-                if (DataContext is Clinic.ViewModels.Admin.AdminUserManagementViewModel vm)
-                {
-                    vm.LoadUsers();
-                }
-            }
+            if (DataContext is AdminUserManagementViewModel vm &&
+                (sender as DataGrid)?.SelectedItem is User user)
+                vm.EditUser(user);
         }
 
 
